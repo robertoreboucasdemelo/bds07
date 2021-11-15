c7ede86bba8e6916a83354177890a2ccb37ac46c
moduretick <robertoreboucasdemelo@gmail.com>
2021-11-14 21:42:18 -0300

    Final Project

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,29 +1,68 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import axios from 'axios';
+
+type FormData = { cep: string };
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+};
 
 const CepSearch = () => {
+  const [address, setAddress] = useState<Address>();
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: '',
+  });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .get(`https://viacep.com.br/ws/${formData.cep}/json`)
+      .then((response) => {
+        setAddress(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+        console.log(error);
+      });
+  };
+
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
+              name="cep"
               type="text"
+              value={formData.cep}
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
             </button>
           </div>
         </form>
-
-        <ResultCard title="Logradouro" description="Lalala" />
-        <ResultCard title="Número" description="234" />
-
+        {address && (
+          <>
+            <ResultCard title="Logradouro" description={address.logradouro} />
+            <ResultCard title="Localidade" description={address.localidade} />
+          </>
+        )}
       </div>
     </div>
   );
